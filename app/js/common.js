5360a14b8ae82816af7bc9e73ae60042ff9d8241
NikolayTydir <vputth1@gmail.com>
2017-02-20 16:37:02 +0200

    Add owlCarousel

--- app/js/common.js
+++ app/js/common.js
@@ -6,7 +6,19 @@ $(function() {
 		return false;
 	});
 
-
+	$(".slider").owlCarousel({
+		items : 1,
+		nav : true,
+		navText : "",
+		loop : true,
+		autoplay : true,
+		autoplayHoverPause : true,
+		fluidSpeed : 600,
+		autoplaySpeed : 600,
+		navSpeed : 600,
+		dotsSpeed : 600,
+		dragEndSpeed : 600
+	});
 	
 	//SVG Fallback
 	if(!Modernizr.svg) {
